6c2d20aeb87a8d9af6a7f15a51c9f883529ea7af
David E. Wheeler <david@justatheory.com>
2011-09-21 23:04:36 -0700

    Fix Github links.

--- lib/Pg/RollDump.pm
+++ lib/Pg/RollDump.pm
@@ -404,9 +404,9 @@ Returns the base file name to be used for the dump file.
 =head1 Support
 
 This module is stored in an open GitHub repository,
-L<http://github.com/theory/pg-rolldump/>. Feel free to fork and contribute!
+L<http://github.com/pgexperts/pg-rolldump/>. Feel free to fork and contribute!
 
-Please file bug reports at L<http://github.com/theory/pg-rolldump/issues/>.
+Please file bug reports at L<http://github.com/pgexperts/pg-rolldump/issues/>.
 
 =head1 Author
 
